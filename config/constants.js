ecfb23a5373df969a8583d9c33e9fff485119d32
kentlan <salsicom@gmail.com>
2018-06-21 20:09:23 +0300

    init

---
+++ config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyA2vxRTwDVaCJVE4CS6RhaScrh0AG7_smY'
+export const FIREBASE_AUTH_DOMAIN = 'by-heart-71ee6.firebaseapp.com'
+export const FIREBASE_DATABASE_URL = 'https://by-heart-71ee6.firebaseio.com'
+export const FIREBASE_PROJECT_ID = 'by-heart-71ee6'
+export const FIREBASE_STORAGE_BUCKET = 'by-heart-71ee6.appspot.com'
+export const FIREBASE_MESSAGING_SENDER_ID = '169132623875'
+
+export const FACEBOOK_APP_ID = '1094496300703576'
